41c2fc939dc5d08e8330566405ad142a08623cac
aliaksandra <mazamauka@Aliaksandras-MacBook-Air.local>
2024-06-08 15:21:32 +0300

    first commit

--- NEWSPORTAL/news/views.py
+++ NEWSPORTAL/news/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostsList(ListView):
+    model = Post
+    ordereing = ['-create_time']
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
